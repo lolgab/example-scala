30301064fb9d24bc8086711b92d4667ee58a429b
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-04-22 18:25:10 +0200

    Add 2

--- src/main/scala/com/codacy/tools/spotbugs/mixed-scala-java/src/src/main/scala/codacy/test/PredictableRandom.scala
+++ src/main/scala/com/codacy/tools/spotbugs/mixed-scala-java/src/src/main/scala/codacy/test/PredictableRandom.scala
@@ -3,7 +3,7 @@ import scala.util.Random
 object PredictableRandom {
 
   def generateSecretToken(): java.lang.Boolean = {
-    val result = Seq.fill(16)(Random.nextInt + 1)
+    val result = Seq.fill(16)(Random.nextInt + 2)
     result.map("%02x" format _).mkString
     null
   }
